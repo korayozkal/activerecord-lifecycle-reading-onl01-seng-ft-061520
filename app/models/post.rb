e64d03b6ca70921f7b8ef08994429f0e6be87b90
Koray Ozkal <koryozkal@gmail.com>
2020-09-02 03:45:45 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,12 @@ class Post < ActiveRecord::Base
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
     end
+  
+  def make_title_case
+  self.title = self.title.titlecase
+end  
+    
+    
   end
 
   def make_title_case
